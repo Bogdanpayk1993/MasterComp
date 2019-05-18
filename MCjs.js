4890c89a90f251d26dc0afe0c6bba6e27f1cdcf0
Bogdanpayk1993 <body.payk.1993@gmail.com>
2019-05-18 13:09:05 +0300

    slider does
    slider does

--- MCjs.js
+++ MCjs.js
@@ -1,15 +1,3 @@
-let i = 1
-let img
-let jmg1 = "./image/phone1.jpg"
-let jmg2 = "./image/phone2.jpg"
-let jmg3 = "./image/phone3.jpg"
-
-window.onload = function () {
-    slider()
-    document.getElementById("facebook").onclick = facebook
-    document.getElementById("instagram").onclick = instagram
-}
-
 function facebook() {
     open('http://www.facebook.com/mastercomp2/')
 }
@@ -18,68 +6,76 @@ function instagram() {
     open('https://www.instagram.com/mastercomp_uzh/?hl=ru')
 }
 
-function slider_prev() {
-    i = i - 1
-
-    if (i == 0)
-        i = 3
-
-    print()
-}
-
-function slider_next() {
-    i = i + 1
-
-    if (i == 4)
-        i = 1
-
-    print()
-}
-
-function print() {
-    switch (i) {
-        case 1:
-            img.src = `${jmg1}`
-            break
-        case 2:
-            img.src = `${jmg2}`
-            break
-        case 3:
-            img.src = `${jmg3}`
-            break
+class Slider {
+    constructor(jmg1, jmg2, jmg3) {
+        this.i = 1
+        this.jmg1 = jmg1
+        this.jmg2 = jmg2
+        this.jmg3 = jmg3
     }
+
+    slider_prev() {
+        this.i = this.i - 1
+
+        if (this.i == 0)
+            this.i = 3
+
+        this.print()
+    }
+
+    slider_next() {
+        this.i = this.i + 1
+
+        if (this.i == 4)
+            this.i = 1
+
+        this.print()
+    }
+
+    print() {
+        switch (this.i) {
+            case 1:
+                this.div.style.backgroundImage = `url(${this.jmg1})`
+                break
+            case 2:
+                this.div.style.backgroundImage = `url(${this.jmg2})`
+                break
+            case 3:
+                this.div.style.backgroundImage = `url(${this.jmg3})`
+                break
+        }
+    }
+
+    render(containerID) {
+        this.div = document.getElementById(containerID)
+        this.div.style.backgroundImage = `url(${this.jmg1})`
+        this.div.style.backgroundSize = "100%"
+        this.div.style.height = "30.8vw"
+
+        let prev = document.createElement("img")
+        prev.src = "./image/prev.png"
+        prev.style.width = "5vw"
+        prev.style.float = "left"
+        prev.style.marginLeft = "3vw"
+        prev.style.marginTop = "12.9vw"
+        prev.onclick = this.slider_prev.bind(this)
+        this.div.appendChild(prev)
+
+        let next = document.createElement("img")
+        next.src = "./image/next.png"
+        next.style.width = "5vw"
+        next.style.float = "right"
+        next.style.marginRight = "3vw"
+        next.style.marginTop = "12.9vw"
+        next.onclick = this.slider_next.bind(this)
+        this.div.appendChild(next)
+    }
+
 }
 
-function slider() {
-    let div = document.getElementById("slider")
-    div.style.height = "30.9vw"
-
-    img = document.createElement("img")
-    img.src = `${jmg1}`
-    img.style.width = "100%"
-    img.style.position = "absolute"
-    img.style.zIndex = "0"
-    div.appendChild(img)
-
-    let prev = document.createElement("img")
-    prev.src = "./image/prev.png"
-    prev.style.width = "5%"
-    prev.style.float = "left"
-    prev.style.marginLeft = "3%"
-    prev.style.position = "relative"
-    prev.style.zIndex = "1"
-    prev.style.marginTop = "12.9vw"
-    prev.onclick = slider_prev
-    div.appendChild(prev)
-
-    let next = document.createElement("img")
-    next.src = "./image/next.png"
-    next.style.width = "5%"
-    next.style.float = "right"
-    next.style.marginRight = "3%"
-    next.style.position = "relative"
-    next.style.zIndex = "1"
-    next.style.marginTop = "12.9vw"
-    next.onclick = slider_next
-    div.appendChild(next)
+window.onload = function () {
+    var slider = new Slider('./image/phone1.jpg','./image/phone2.jpg','./image/phone3.jpg')
+    slider.render("slider")
+    document.getElementById("facebook").onclick = facebook
+    document.getElementById("instagram").onclick = instagram
 }
